6b5fb5839620d907177b5a8e64c9229a58d673cf
Matt Lambert <50882540+MrLambs@users.noreply.github.com>
2020-09-20 17:45:29 -0700

    Update render
    Introduced React.lazy()

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import React from "react";
-import "./App.css";
-
-import Nav from "./Components/Nav/Nav";
-import Home from "./Components/Home/Home";
-import Commands from "./Components/Commands/Commands";
-import Contact from './Components/Contact/Contact'
-
+import React, { lazy, Suspense } from "react";
 import { Switch, Route } from "react-router";
+import "./App.css";
+// import Home from "./Components/Home/Home";
+// import Commands from "./Components/Commands/Commands";
+// import Contact from './Components/Contact/Contact'
+// import Nav from "./Components/Nav/Nav";
+
+const Nav = lazy(() => import('./Components/Nav/Nav'));
+const Home = lazy(() => import('./Components/Home/Home'))
+const Commands = lazy(() => import('./Components/Commands/Commands'))
+const Contact = lazy(() => import('./Components/Contact/Contact'))
+
+const renderLoader = () => <p>Loading...</p>
+
+
 
 function App() {
   return (
     <div className="App">
-      <Nav />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/commands" component={Commands} />
-        <Route path="/contact" component={Contact} />
-      </Switch>
+      <Suspense fallback={renderLoader}>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/commands" component={Commands} />
+          <Route path="/contact" component={Contact} />
+        </Switch>
+      </Suspense>
     </div>
   );
 }
--- src/Components/Commands/Scrollspy/Scrollspy.js
+++ src/Components/Commands/Scrollspy/Scrollspy.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import M from "materialize-css";
 import "materialize-css/dist/css/materialize.min.css";
 
-
 import Casino from '../Sections/Casino';
 import Forts from '../Sections/Forts';
 import Images from '../Sections/Images';
